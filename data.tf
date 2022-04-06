e90438dbc6b43b16e7812230ecbf75255f9a4878
Sachin Kumar <sachin.kumar@awaze.co.uk>
2022-04-06 17:01:57 +0100

    fix: first commit with files

---
+++ data.tf
@@ -0,0 +1,9 @@
+################################################################################
+# Supporting Resources
+################################################################################
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_elb_service_account" "current" {}
