8d5d1c0a0bb17a9935b5e0c87024d95d7ffb477e
sachinyadav100 <103128862+sachinyadav100@users.noreply.github.com>
2022-04-09 00:13:21 +0100

    Update data.tf

--- data.tf
+++ data.tf
@@ -7,3 +7,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 data "aws_elb_service_account" "current" {}
+
+data "aws_ecr_repository" "ecr" {
+  name = var.ecr_repo_name
+}
